881f9edcfe27aee2f635ca7a996e9963ca913251
Toggle <idenhan129@gmail.com>
2021-01-12 15:31:26 +0900

    Build component interface

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
 import Like from "./common/like";
+import Pagination from "./common/pagination";
 
 // JSX, Rendering Lists, Conditional Rendering, Handling Events, Updating the State
 class Movies extends Component {
-  state = { movies: getMovies() };
+  state = { movies: getMovies(), pageSize: 4 };
 
   handleDelete = movie => {
     const movies = this.state.movies.filter(m => m._id !== movie._id);
@@ -18,6 +19,10 @@ class Movies extends Component {
     movies[index].liked = !movies[index].liked;
 
     this.setState({ movies });
+  };
+
+  handlePageChange = page => {
+    console.log(page);
   };
 
   render() {
@@ -64,6 +69,11 @@ class Movies extends Component {
             ))}
           </tbody>
         </table>
+        <Pagination
+          itemsCount={count}
+          pageSize={this.state.pageSize}
+          onPageChange={this.handlePageChange}
+        />
       </React.Fragment>
     );
   }
